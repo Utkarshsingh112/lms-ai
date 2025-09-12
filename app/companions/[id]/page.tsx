2594bac44a79dc3da5d3335512b06504ce2214ac
NikT404 <nikhilsinghrajawat0@gmail.com>
2025-09-12 21:59:59 +0530

    initial commit

---
+++ app/companions/[id]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const CompanionSession = () => {
+  return (
+    <div>CompanionSession</div>
+
+  )
+}
+
+export default CompanionSession
---
+++ app/companions/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Companionslibrary = () => {
+  return (
+    <div>Companionslibrary</div>
+  )
+}
+
+export default Companionslibrary
