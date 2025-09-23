68b8ecbb861045c9168d38b3fea19bd87870eabd
NikT404 <nikhilsinghrajawat0@gmail.com>
2025-09-23 16:28:27 +0530

    added companion library cards

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { getAllCompanions } from "@/lib/actions/companions.action";
+import CompanionCard from "@/components/CompanionCard";
+import { getSubjectsColor } from "@/lib/utils";
+import SearchInput from "@/components/SearchInput";
+import SubjectFilter from "@/components/SubjectFilter";
 
-const Companionslibrary = () => {
-  return (
-    <div>Companionslibrary</div>
+
+//witht the help of this we can print the data and object locally in the terminal
+const Companionslibrary = async ({ searchParams }: SearchParams) => {
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.topic : "";
+  const topic = filters.topic ? filters.topic : "";
+
+  const companions = await getAllCompanions({ subject, topic });
+  console.log(companions);
+  return(
+     <main>
+            <section className="flex justify-between gap-4 max-sm:flex-col">
+                <h1>Companion Library</h1>
+                <div className="flex gap-4">
+                    <SearchInput/>
+                    <SubjectFilter/>
+                </div>
+            </section>
+            <section className="companions-grid  ">
+                {companions.map((companion) => (
+                    <CompanionCard
+                        key={companion.id}
+                        {...companion}
+                        color={getSubjectsColor(companion.subject)}
+                    />
+                ))}
+            </section>
+        </main>
   )
-}
 
-export default Companionslibrary
+};
+
+export default Companionslibrary;
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,5 @@
 import { clsx, type ClassValue } from "clsx"
+
 import { twMerge } from "tailwind-merge"
 import { subjectsColors } from "@/constants"
 export function cn(...inputs: ClassValue[]) {
